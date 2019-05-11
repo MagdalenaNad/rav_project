ed18b80b3432a2cd24890d49e0f96b2999530838
Dariusz.Żulpo <dd2000@wp.pl>
2019-05-11 11:14:00 +0200

    root: Vehicles + subclasses: Cars, MotorBoats, Amphibians

--- src/main/java/pl/sda/rav/Vehicles.java
+++ src/main/java/pl/sda/rav/Vehicles.java
@@ -8,4 +8,9 @@ public class Vehicles {  // pojazdy
     private String name; // nazwa
     private LocalDate productionDate;  // data produkcji
 
+    public Vehicles(String vin, String name, LocalDate productionDate) {
+        this.vin = vin;
+        this.name = name;
+        this.productionDate = productionDate;
+    }
 }  // class Vehicles
