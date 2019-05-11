973e244e75a9624c56d6bc688a40daccda319255
MagdalenaNad <m_nadratowska@o2.pl>
2019-05-11 13:09:24 +0200

    OrderPeriod test

--- src/main/java/pl/sda/rav/OrdersDao.java
+++ src/main/java/pl/sda/rav/OrdersDao.java
@@ -1,19 +1,22 @@
 package pl.sda.rav;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrdersDao {
 
-    private List<Order> orders;
+    private List<Order> orders= new ArrayList<>();
 
-    public void makeOrder(User customer, Vehicles vehicles,OrderPeriod orderPeriod ){
+    public void makeOrder(String customer, Vehicles vehicles,OrderPeriod orderPeriod ){
         orders.add(new Order(customer,vehicles, orderPeriod));
     }
 
     public boolean isAvalible(String vin, LocalDate start, LocalDate stop){
         for (Order order : orders) {
-            if (order.getVehicle().getVin().equals(vin)) return !order.getOrderPeriod().isIncluded(start,stop);
+            if (order.getVehicle().getVin().equals(vin)) {
+                if(order.getOrderPeriod().isIncluded(start, stop)) return false;
+            }
         }
         return true;
     }
--- src/test/java/pl/sda/rav/OrdersDaoTest.java
+++ src/test/java/pl/sda/rav/OrdersDaoTest.java
@@ -1,7 +1,9 @@
 package pl.sda.rav;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,7 +15,26 @@ class OrdersDaoTest {
     void shouldAvalible() {
 
         //given
+        OrdersDao ordersDao = new OrdersDao();
         List<Order> orders =new ArrayList<>();
-      //  orders.add(new Order("Pan", new Cars("MA84","Ford", 1987, )))
+        orders.add(new Order("Pan", new Cars("MA84","Ford", 1987,"kombi", 400), new OrderPeriod(LocalDate.of(2019,4, 10), LocalDate.of(2019, 4, 18)) ));
+        orders.add(new Order("Pan", new Cars("MA65","Ford", 1967,"kombi", 400), new OrderPeriod(LocalDate.of(2019,3, 26), LocalDate.of(2019, 4, 3)) ));
+        LocalDate t1 = LocalDate.of(2019, 4, 1);
+        LocalDate t2 = LocalDate.of(2019, 4, 9);
+        LocalDate t3 = LocalDate.of(2019, 4, 13);
+
+        //when
+        boolean shouldBeAvalible = ordersDao.isAvalible("MA84", t1, t2);
+        boolean shouldBeNotAvalible = ordersDao.isAvalible("MA65", t1, t2);
+        boolean shouldBeAvalible1 = ordersDao.isAvalible("MA65", t2, t3);
+        boolean shouldBeNotAvalible2 = ordersDao.isAvalible("MA84", t1, t3);
+
+        //then
+        Assertions.assertTrue(shouldBeAvalible);
+        Assertions.assertFalse(shouldBeNotAvalible);
+        Assertions.assertTrue(shouldBeAvalible1);
+        Assertions.assertFalse(shouldBeNotAvalible2);
+
+
     }
 }
