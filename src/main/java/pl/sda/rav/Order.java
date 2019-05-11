2b64130d0d0bec283d23cb4852177d55e9c7ebd7
MagdalenaNad <m_nadratowska@o2.pl>
2019-05-11 11:10:07 +0200

    order + orderPeriod

--- src/main/java/pl/sda/rav/Order.java
+++ src/main/java/pl/sda/rav/Order.java
@@ -1,4 +1,42 @@
 package pl.sda.rav;
 
 public class Order {
+    private static int COUNT=1;
+    private int id;
+    private User customer;
+    private Vehicles vehicle;
+    private OrderPeriod orderPeriod;
+
+    public Order(User customer, Vehicles vehicle, OrderPeriod orderPeriod) {
+        this.id = COUNT++;
+        this.customer = customer;
+        this.vehicle = vehicle;
+        this.orderPeriod = orderPeriod;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public User getCustomer() {
+        return customer;
+    }
+
+    public Vehicles getVehicle() {
+        return vehicle;
+    }
+
+    public OrderPeriod getOrderPeriod() {
+        return orderPeriod;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id: " + id +
+                ", custome: " + customer +
+                ", vehicle: " + vehicle +
+                ", orderPeriod: " + orderPeriod +
+                '}';
+    }
 }
